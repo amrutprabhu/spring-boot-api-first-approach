579ed3bfa37b5846af70c500ab08cff59cca8567
amrutprabhu <amrutprabhu42@gmail.com>
2022-07-23 16:10:51 +0200

    Fixing server project

--- api-first-server/src/main/java/com/amrut/prabhu/AccountServiceImpl.java
+++ api-first-server/src/main/java/com/amrut/prabhu/AccountServiceImpl.java
@@ -1,8 +1,8 @@
 package com.amrut.prabhu;
 
 import com.amrut.prabhu.api.AccountApiDelegate;
-import com.amrut.prabhu.model.InlineObject;
-import com.amrut.prabhu.model.InlineResponse200;
+import com.amrut.prabhu.model.CreateAccount200Response;
+import com.amrut.prabhu.model.CreateAccountRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,9 +15,9 @@ import java.util.UUID;
 public class AccountServiceImpl implements AccountApiDelegate {
 
     @Override
-    public ResponseEntity<InlineResponse200> createAccount(InlineObject request) {
+    public ResponseEntity<CreateAccount200Response> createAccount(CreateAccountRequest request) {
         log.info("Incoming request :{}", request);
-        InlineResponse200 response = new InlineResponse200();
+        CreateAccount200Response response = new CreateAccount200Response();
         response.accountId(UUID.randomUUID().toString())
                 .accountType(request.getAccountType())
                 .name(request.getName());
