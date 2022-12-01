c9e35d4b9374032c1a82372169d83e8f10775949
Amrut Prabhu <amrutprabhu42@gmail.com>
2022-12-01 21:54:35 +0100

    Updating to java 17 and Adding tests. (#37)
    * Updating to java 17 and Adding tests.
    
    * updated pipeline

--- api-first-client/src/main/java/com/amrut/prabhu/Config.java
+++ api-first-client/src/main/java/com/amrut/prabhu/Config.java
@@ -1,15 +1,27 @@
 package com.amrut.prabhu;
 
+import com.amrut.prabhu.api.AccountApi;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+                .build();
+    }
 
     @Bean
-    public RestTemplate accountApi() {
-        return new RestTemplate();
+    @Primary
+    public ApiClient apiclient(RestTemplate restTemplate) {
+        ApiClient apiclient = new ApiClient(restTemplate);
+        apiclient.setBasePath("http://localhost:8080");
+        return apiclient;
+
     }
 }
